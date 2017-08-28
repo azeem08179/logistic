21e378e82565f1eccba7e84ba1a1d47d471312ec
azeem08179 <azeem08179@gmail.com>
2017-08-28 10:56:36 +0500

    second commit

--- logistic_predict.py
+++ logistic_predict.py
@@ -1,11 +1,11 @@
 import numpy as np 
 from process import get_binary_data
 
-X, Y = get_binary_data()
+X, Y = get_binary_data() # data after one hot encoding
 
-D = X.shape[1]
+D = X.shape[1] # D is the no of features
 
-w = np.random.randn(D)
+w = np.random.randn(D) # w is a cloumn vector
 
 b = 0
 
@@ -17,8 +17,10 @@ def sigmoid(a):
 def forward(X, w, b):
 	return sigmoid(X.dot(w) + b)
 
-P_Y_given_X = forward(X, w, b)	
+P_Y_given_X = forward(X, w, b)
+print("P_Y_given_X=\n"),P_Y_given_X[1:10]	
 predictions = np.round(P_Y_given_X)
+print("predictions=\n"),predictions[1:10]
 
 def classification_rate(Y,P):
 	return np.mean(Y == P)
--- process.py
+++ process.py
@@ -1,10 +1,13 @@
 import numpy as np 
 import pandas as pd 
+from colorama import Fore, Back, Style  #pip install colorama - to change the text color 
 
 def get_data():
 
     df = pd.read_csv('ecommerce_data.csv')
-    print "###### Loading Data ...... ##############\n", df.head()
+    print(Fore.RED + '###### Loading Data ...... ##############\n')
+    print(Style.RESET_ALL)
+    print df.head()
     data = df.as_matrix()
 
     X = data[:, :-1] # copying all elements except last column
@@ -27,23 +30,28 @@ def get_data():
     X2[:,0:(D-1)] = X[:,0:(D-1)]  # non-categorical  D=5
 
     print "\nX2 = ", np.array_str(X2[1,:], precision=2)
-
-    for n in xrange(N): # one hot encoding for time of the day variable
-        t = int(X[n,D-1]) # D-1 = 4  4th coloumn is the time of the day
-        X2[n,t+D-1] = 1  # columns will be 4, 5 , 6 , 7
-
+    print("X2 (time of the day coloumn):"),X[1:50,4]
+    # one hot encoding for time of the day variable
+    # D-1 = 4  4th coloumn is the time of the day
+    # columns will be 4, 5 , 6 , 7
+    for n in xrange(N):
+       t = int(X[n,D-1])
+       X2[n,t+D-1] = 1
+        
         #Z = np.zeros((N,4))
         #Z[np.arrange(N),X[:,D-1].astype(np.int32)] = 1
 
         #assert(np.abs(X2[:,-4:])- Z).sum() < 10e-10 )
+    print(Fore.RED + '***** X2 sample after one hot encoding:****\n')
+    print(Style.RESET_ALL)
+    print X2[1:20,4:8] # debug  
+    return X2, Y
 
-        return X2, Y
 
 
 def get_binary_data():
     X, Y = get_data()
     X2 = X[Y <= 1]
-    Y2 = Y[Y <= 1]
-
+    Y2 = Y[Y <= 1]  
     return X2, Y2
 
